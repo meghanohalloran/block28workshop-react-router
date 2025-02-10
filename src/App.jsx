05ff0ea65fa6bf6f210f640768262b3bcc0f698f
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-09 22:48:22 -0700

    react router configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-const App = () => {
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; 
 
+const App = () => {
   return (
-  <>
-  <h1>after cleanup of vite react </h1>
-  </>
-  )
-  }
-  export default App
-  
+    <div className="main-container">
+        <div id="navbar">
+          {/* Navbar Links will go here later */}
+      </div>
+      
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<h1>Blue</h1>} />
+          <Route path="/red" element={<h1>Red</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
